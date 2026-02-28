e078e1191bbd53898168333e47b1802c615c0dff
maxkondaurov <maxkondaurov2@proton.me>
2026-02-28 18:57:57 +0300

    more adding

--- views/users/views.py
+++ views/users/views.py
@@ -1,9 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, BackgroundTasks
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper, User
+from utils.email_sender import send_email
 from views.users.schemas import UserSchema, UserSchemaUpdate, UserSchemaIn
 from . import crud
 from .dependencies import get_user_by_id_or_raise
@@ -15,8 +16,23 @@ async def get_users_list(session: Annotated[AsyncSession,Depends(db_helper.get_s
     return await crud.get_users(session = session)
 
 @router.post('/', response_model=UserSchema)
-async def create_user(data_create: UserSchemaIn,session: Annotated[AsyncSession,Depends(db_helper.get_session_dependency)]):
-    return await crud.create_user(session = session, data_create = data_create)
+async def create_user(
+    background_tasks: BackgroundTasks,
+    # session: Annotated[AsyncSession, Depends(db_helper.get_session_dependency)],
+    data_create: UserSchema,
+    session: AsyncSession = Depends(db_helper.get_session_dependency),
+):
+    user = await crud.create_user(
+        session=session,
+        data_create = data_create,
+    )
+    background_tasks.add_task(
+        send_email,
+        user.email,
+        f"Hello dear {user.username}",
+        text="Welcome message.",
+    )
+    return user
 
 
 @router.get('/{user_id}', response_model=UserSchema)
